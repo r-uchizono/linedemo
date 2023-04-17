ce8ba44c582bfd0890ffcbffbfc342f08d46a1e5
内薗 <r-uchizono@unis-soft.co.jp>
2023-04-17 17:24:13 +0900

    検証

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ server.post('/bot/webhook', middleware(line_config), (req, res, next) => {
     // すべてのイベント処理のプロミスを格納する配列。
      let events_processed = [];
 
-      fetch("yoyaku.json")
+     let url = 'yoyaku.json';
+      fetch(url)
       .then(res => res.json)
       .then(data => {
           req.body.events.forEach((event) => {
