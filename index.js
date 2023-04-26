605f8716bb273e26e72a0bc287f80a83a532ec4d
内薗 <r-uchizono@unis-soft.co.jp>
2023-04-26 16:50:34 +0900

    test

--- index.js
+++ index.js
@@ -129,11 +129,11 @@ app.post('/bot/webhook', middleware(line_config), (req, res, next) => {
                                 const s_formattedDate = `${s_year}年${s_month}月${s_day}日（${s_dayOfWeek}）`;
 
                                 let firstEventJson = JSON.parse(dataJSON);
-                                templateJson.header.contents[0].text = event_nm + '/' + res.rows[0].kaisaiti_nm + '会場';
-                                templateJson.body.contents[0].text = f_formattedDate;
-                                templateJson.body.contents[1].text = '開催時間　' + F_SformattedTime + '～' + F_EformattedTime;
-                                templateJson.body.contents[2].text = '場所　' + res.rows[0].place_name;
-                                templateJson.body.contents[3].text = '　　　' + res.rows[0].place_address;
+                                firstEventJson.header.contents[0].text = event_nm + '/' + res.rows[0].kaisaiti_nm + '会場';
+                                firstEventJson.body.contents[0].text = f_formattedDate;
+                                firstEventJson.body.contents[1].text = '開催時間　' + F_SformattedTime + '～' + F_EformattedTime;
+                                firstEventJson.body.contents[2].text = '場所　' + res.rows[0].place_name;
+                                firstEventJson.body.contents[3].text = '　　　' + res.rows[0].place_address;
                                 data.contents.push(firstEventJson);
 
                                 let secondEventJson = JSON.parse(dataJSON);
