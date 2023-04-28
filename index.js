6be8bfd3baaac4f43cd067dbdd9ded3bf7ac688d
内薗 <r-uchizono@unis-soft.co.jp>
2023-04-28 10:38:32 +0900

    test

--- index.js
+++ index.js
@@ -264,7 +264,7 @@ app.post('/bot/webhook', middleware(line_config), (req, res, next) => {
             // DB登録処理
             const query = {
                 text: 'INSERT INTO t_yoyaku(event_cd, kaisaiti_cd, user_id, reserve_time) VALUES($1, $2, $3, $4)',
-                values: [event.postback.data.split('=')[1], '2023B',event.source.userId, event.postback.params.time],
+                values: [event.postback.data.split('=')[1], 2023 ,event.source.userId, event.postback.params.time],
             }
 
             console.log(event.postback.data);
