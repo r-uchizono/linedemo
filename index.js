bc812e9963c6ac02e9549ebfbad3da70980541d6
内薗 <r-uchizono@unis-soft.co.jp>
2023-05-08 13:57:02 +0900

    test

--- index.js
+++ index.js
@@ -227,42 +227,42 @@ app.post('/bot/webhook', middleware(line_config), (req, res, next) => {
 
 
                                
-                                const { JSDOM } = jsdom;
+                                // const { JSDOM } = jsdom;
 
-                                const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-                                const document = dom.window.document;
+                                // const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+                                // const document = dom.window.document;
                              
-                                const ctx = document.getElementById('myChart');
+                                // const ctx = document.getElementById('myChart');
 
-                                const chart = new Chart(ctx, {
-                                    type: 'bar',
-                                    data: {
-                                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                                        datasets: [{
-                                            label: '# of Votes',
-                                            data: [12, 19, 3, 5, 2, 3],
-                                            backgroundColor: [
-                                                'rgba(255, 99, 132, 0.2)',
-                                                'rgba(54, 162, 235, 0.2)',
-                                                'rgba(255, 206, 86, 0.2)',
-                                                'rgba(75, 192, 192, 0.2)',
-                                                'rgba(153, 102, 255, 0.2)',
-                                                'rgba(255, 159, 64, 0.2)'
-                                            ],
-                                            borderColor: [
-                                                'rgba(255, 99, 132, 1)',
-                                                'rgba(54, 162, 235, 1)',
-                                                'rgba(255, 206, 86, 1)',
-                                                'rgba(75, 192, 192, 1)',
-                                                'rgba(153, 102, 255, 1)',
-                                                'rgba(255, 159, 64, 1)'
-                                            ],
-                                            borderWidth: 1
-                                        }]
-                                    }
-                                })
+                                // const chart = new Chart(ctx, {
+                                //     type: 'bar',
+                                //     data: {
+                                //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                                //         datasets: [{
+                                //             label: '# of Votes',
+                                //             data: [12, 19, 3, 5, 2, 3],
+                                //             backgroundColor: [
+                                //                 'rgba(255, 99, 132, 0.2)',
+                                //                 'rgba(54, 162, 235, 0.2)',
+                                //                 'rgba(255, 206, 86, 0.2)',
+                                //                 'rgba(75, 192, 192, 0.2)',
+                                //                 'rgba(153, 102, 255, 0.2)',
+                                //                 'rgba(255, 159, 64, 0.2)'
+                                //             ],
+                                //             borderColor: [
+                                //                 'rgba(255, 99, 132, 1)',
+                                //                 'rgba(54, 162, 235, 1)',
+                                //                 'rgba(255, 206, 86, 1)',
+                                //                 'rgba(75, 192, 192, 1)',
+                                //                 'rgba(153, 102, 255, 1)',
+                                //                 'rgba(255, 159, 64, 1)'
+                                //             ],
+                                //             borderWidth: 1
+                                //         }]
+                                //     }
+                                // })
 
-                                console.log(chart)
+                                // console.log(chart)
                                 
                                 
 
