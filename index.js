0d94a220e745548ce9ce7d9abee5b232a1074f20
内薗 <r-uchizono@unis-soft.co.jp>
2023-05-08 17:13:42 +0900

    test

--- index.js
+++ index.js
@@ -253,41 +253,41 @@ app.post('/bot/webhook', middleware(line_config), (req, res, next) => {
     
     
                                    
-                                    // let { JSDOM } = jsdom;
+                                    let { JSDOM } = jsdom;
     
-                                    // let dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-                                    // let document = dom.window.document;
+                                    let dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+                                    let document = dom.window.document;
                                  
-                                    // let ctx = document.getElementById('myChart');
+                                    // let ctx = ;
     
-                                    //         let chart = new Chart(ctx, {
-                                    //             type: 'bar',
-                                    //             data: {
-                                    //                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                                    //                 datasets: [{
-                                    //                     label: '# of Votes',
-                                    //                     data: [12, 19, 3, 5, 2, 3],
-                                    //                     backgroundColor: [
-                                    //                         'rgba(255, 99, 132, 0.2)',
-                                    //                         'rgba(54, 162, 235, 0.2)',
-                                    //                         'rgba(255, 206, 86, 0.2)',
-                                    //                         'rgba(75, 192, 192, 0.2)',
-                                    //                         'rgba(153, 102, 255, 0.2)',
-                                    //                         'rgba(255, 159, 64, 0.2)'
-                                    //                     ],
-                                    //                     borderColor: [
-                                    //                         'rgba(255, 99, 132, 1)',
-                                    //                         'rgba(54, 162, 235, 1)',
-                                    //                         'rgba(255, 206, 86, 1)',
-                                    //                         'rgba(75, 192, 192, 1)',
-                                    //                         'rgba(153, 102, 255, 1)',
-                                    //                         'rgba(255, 159, 64, 1)'
-                                    //                     ],
-                                    //                     borderWidth: 1
-                                    //                 }]
-                                    //             }
-                                    //         })
-                                    //         console.log(chart)                              
+                                    let chart = new Chart(document.getElementById('myChart'), {
+                                        type: 'bar',
+                                        data: {
+                                            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                                            datasets: [{
+                                                label: '# of Votes',
+                                                data: [12, 19, 3, 5, 2, 3],
+                                                backgroundColor: [
+                                                    'rgba(255, 99, 132, 0.2)',
+                                                    'rgba(54, 162, 235, 0.2)',
+                                                    'rgba(255, 206, 86, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)',
+                                                    'rgba(153, 102, 255, 0.2)',
+                                                    'rgba(255, 159, 64, 0.2)'
+                                                ],
+                                                borderColor: [
+                                                    'rgba(255, 99, 132, 1)',
+                                                    'rgba(54, 162, 235, 1)',
+                                                    'rgba(255, 206, 86, 1)',
+                                                    'rgba(75, 192, 192, 1)',
+                                                    'rgba(153, 102, 255, 1)',
+                                                    'rgba(255, 159, 64, 1)'
+                                                ],
+                                                borderWidth: 1
+                                            }]
+                                        }
+                                    })
+                                    console.log(chart)                              
                                     
     
                                     
