3e38e31a3ad241c81776ccc518530485e53e128c
内薗 <r-uchizono@unis-soft.co.jp>
2023-05-17 11:07:02 +0900

    test

--- index.js
+++ index.js
@@ -49,21 +49,21 @@ const bot = new line.Client(line_config)
 //     ssl: true 
 // })
 
-// const client = new pg.Pool({
-//     user: process.env.PG_USER,
-//     host: process.env.PG_HOST,
-//     database: process.env.PG_DBNM,
-//     password: process.env.PG_PSWD,
-//     port: process.env.PG_PORT
-// })
-
 const client = new pg.Pool({
-    user: 'unis',
-    host: 'dpg-cgvn4qodh87joksvpj70-a',
-    database: 'event_f91d',
-    password: 'gbFeZ4j0o2mXOlCdCw0qF4TMaYTkldcn',
-    port: 5432 
-}) 
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DBNM,
+    password: process.env.PG_PSWD,
+    port: process.env.PG_PORT
+})
+
+// const client = new pg.Pool({
+//     user: 'unis',
+//     host: 'dpg-cgvn4qodh87joksvpj70-a',
+//     database: 'event_f91d',
+//     password: 'gbFeZ4j0o2mXOlCdCw0qF4TMaYTkldcn',
+//     port: 5432 
+// }) 
 
 const FORMAT = 'YYYY/MM/DD HH:mm:ss'
 const TIME_ZONE_TOKYO = 'Asia/Tokyo'
