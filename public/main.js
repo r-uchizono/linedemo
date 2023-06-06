bc223475c368deac3e7a99afeb47521a73934e40
watanabe <k-watanabe@unis-soft.co.jp>
2023-06-06 13:19:23 +0900

    mmmm

--- public/main.js
+++ public/main.js
@@ -1,11 +1,51 @@
-import { parse } from 'querystring'
 
 // 読込
 window.onload = () => {
-    const params = parse(window.location.search.slice(1));
+    var urlQuery = (function () {
+        var queryString, queryItems, queryItem,
+            i, length, matchs, key, pkey, skey, value, list, hash, params = {};
+
+        // クエリストリングの取得
+        queryString = window.location.search || '';
+        queryString = queryString.substr(1, queryString.length);
+
+        // パラメター毎に分解
+        queryItems = queryString.split('&');
+
+        // 各パラメターをキー&バリューに分解
+        for (i = 0, length = queryItems.length; i < length; i++) {
+            // 1組取り出し
+            queryItem = (queryItems[i] || '').split('=');
+
+            // キー&バリューに分解
+            key = queryItem[0];
+            value = queryItem[1] ? window.decodeURIComponent(queryItem[1]) : undefined;
+
+            // キー文字列によってオブジェクトの作り方を変える
+            matchs = (/([\w$]*)\[([\w$]*)\]/g).exec(key);
+            if (matchs === null) {
+                // 単純なキー&バリュー
+                params[key] = value;
+            } else {
+                pkey = matchs[1];
+                skey = matchs[2];
+                if (!skey) {
+                    // 配列にバリューを格納
+                    list = params[pkey] = params[pkey] || [];
+                    list[list.length] = value;
+                } else {
+                    // ハッシュにサブキーとバリューを格納
+                    hash = params[pkey] = params[pkey] || {};
+                    hash[skey] = value;
+                }
+            }
+        }
+
+        return params;
+    })();
 
     const test = document.getElementById('test');
-    test.value = params;
+    test.value = urlQuery;
     //const myLiffId = '1660891355-wrO0ydxA';
     const myLiffId = '1660863634-BnGNVK4d';
 
