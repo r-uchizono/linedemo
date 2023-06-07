b26dcab349d28f9ccd453467ab3776fc8316773d
内薗 <r-uchizono@unis-soft.co.jp>
2023-06-07 10:36:17 +0900

    予約時間

--- index.js
+++ index.js
@@ -56,8 +56,7 @@ const client = new pg.Pool({
     host: process.env.PG_HOST,
     database: process.env.PG_DBNM,
     password: process.env.PG_PSWD,
-    port: process.env.PG_PORT,
-    ssl: true
+    port: process.env.PG_PORT
 })
 
 app.post("/", (req, res) => {
