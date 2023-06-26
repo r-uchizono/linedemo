c44f75081fb7804b14a018e48cf86badbaa24002
watanabe <k-watanabe@unis-soft.co.jp>
2023-06-26 15:10:16 +0900

    fafa

--- public/main.js
+++ public/main.js
@@ -54,7 +54,8 @@ window.onload = () => {
     }).then(() => {
       //idトークンによる年齢情報の取得
       const idToken = liff.getIDToken();
-      console.log("urlQuery.kain_cd：", urlQuery.kain_cd);
+      console.log("testLog");
+      console.log("urlQuery.kain_cd：" + urlQuery.kain_cd);
       //if (!urlQuery.kain_cd) {
         const jsonData = JSON.stringify({
           id_token: idToken
