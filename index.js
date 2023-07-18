92a9d0380057d3d189809b08f64eca986c936824
watanabe <k-watanabe@unis-soft.co.jp>
2023-07-18 16:25:49 +0900

    dadada

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ import { confirm, cancel, change } from './confirm.mjs'
 import { id } from './id.mjs'
 import { info } from './info.mjs'
 import { held } from './held.mjs'
+import log4js from 'log4js'
+
+const logger = log4js.getLogger()
+logger.level = 'all'
+
+logger.trace('Some trace messages')
+logger.debug('Some debug messages')
+logger.info('Some info messages')
+logger.warn('Some warn messages')
+logger.error('Some error messages')
+logger.fatal('Some fatal messages')
+
+log4js.shutdown((err) => {
+  if (err) throw err
+  process.exit(0)
+})
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
